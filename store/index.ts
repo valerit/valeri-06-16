11fcc934deeb0113465ba00f19c3b78a5c5fc44a
valeri <valeritsert@gmail.com>
2021-06-15 10:24:50 +0400

    store/index: add sells and buys

--- store/index.ts
+++ store/index.ts
@@ -1,10 +1,14 @@
 import { makeAutoObservable } from "mobx";
+import { Order } from "../types";
 
 class Store {
+  sells: Order[] = [];
+  buys: Order[] = [];
+
   constructor() {
-    makeAutoObservable(this)
+    makeAutoObservable(this);
   }
 }
 
-// export instance 
-export default new Store()
+// export instance
+export default new Store();
