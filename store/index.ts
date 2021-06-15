bd23430a38b026a1935faa9c7c17ca3b9496d30f
valeri <valeritsert@gmail.com>
2021-06-15 10:34:32 +0400

    store/index: subscribe

--- store/index.ts
+++ store/index.ts
@@ -4,9 +4,35 @@ import { Order } from "../types";
 class Store {
   bids: Order[] = [];
   asks: Order[] = [];
+  ws: WebSocket | null = null;
 
   constructor() {
     makeAutoObservable(this);
+  }
+
+  subscribe() {
+    var ws = new WebSocket("wss://www.cryptofacilities.com/ws/v1");
+    this.ws = ws;
+
+    ws.onopen = () => {
+      // connection opened
+      // send a message
+      ws.send(
+        '{ "event": "subscribe", "feed": "book_ui_1", "product_ids": ["PI_XBTUSD"]}'
+      );
+    };
+
+    ws.onmessage = (e) => {
+      // a message was received
+      console.log(e.data);
+    };
+
+    ws.onerror = (e) => {};
+
+    ws.onclose = (e) => {
+      // connection closed
+      console.log(e.code, e.reason);
+    };
   }
 }
 
