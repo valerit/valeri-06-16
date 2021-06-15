96b8ffb00d788f6390477790ee5dc32a0211c8c1
valeri <valeritsert@gmail.com>
2021-06-15 10:43:43 +0400

    types: OrderMessage

--- types.ts
+++ types.ts
@@ -2,4 +2,11 @@ export type Order = {
   price: number;
   size: number;
   total: number;
-}
+};
+
+export type OrderMessage = {
+  feed: string;
+  product_id: string;
+  bids: Array<Array<number>>;
+  asks: Array<Array<number>>;
+};
