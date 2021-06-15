16fa5a14e8029db05fae193bb1ab1419c63f2d65
valeri <valeritsert@gmail.com>
2021-06-16 02:02:22 +0400

    components/OrderList: renderItem

--- components/OrderList.tsx
+++ components/OrderList.tsx
@@ -1,14 +1,27 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList, ListRenderItem } from "react-native";
+import { Order } from "../types";
 
 export type OrderListProps = {
   style: object;
+  data: Array<Order>;
 };
 
-export default function OrderList({ style }: OrderListProps) {
+const renderItem: ListRenderItem<Order> = ({ item }) => {
+  const { price, size, total } = item;
+  return (
+    <View style={styles.item}>
+      <Text>{price}</Text>
+      <Text>{size}</Text>
+      <Text>{total}</Text>
+    </View>
+  );
+};
+
+export default function OrderList({ style, data }: OrderListProps) {
   return (
     <View style={[styles.container, style]}>
-      <Text>OrderList</Text>
+      <FlatList renderItem={renderItem} data={data} />
     </View>
   );
 }
@@ -19,5 +32,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  item: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
