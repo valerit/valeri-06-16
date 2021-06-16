d135b5e7f86b3290767e8cde8f29b5cb68ed3e30
valeri <valeritsert@gmail.com>
2021-06-16 22:02:02 +0400

    store/store.spec: fix tests for refresh

--- store/store.spec.tsx
+++ store/store.spec.tsx
@@ -1,7 +1,31 @@
 import Store from "./index";
+Store.groupOffset = 1;
+
+const msg = `{
+  "feed":"book_ui_1",
+  "product_id":"PI_XBTUSD",
+  "bids":[[1, 1],[1.5, 1], [3, 2], [3.5, 3]],
+  "asks":[[1, 1],[1.5, 1], [4, 3], [4.5, 4]]
+}`;
 
 describe("Store", () => {
+  it("should get a correct group price", () => {
+    const groupPrice = Store.getGroupPrice(1.5);
+
+    expect(groupPrice).toBe(1);
+  });
+
   it("should group data correctly", () => {
-    expect(true).toBe(true);
+    const data = JSON.parse(msg);
+    Store.onOrderMsg(data);
+    Store.refresh();
+
+    expect(Store.bids.length).toBe(2);
+    expect(Store.bids[0].price).toBe(3);
+    expect(Store.bids[0].size).toBe(5);
+
+    expect(Store.asks.length).toBe(2);
+    expect(Store.asks[0].price).toBe(1);
+    expect(Store.asks[0].size).toBe(2);
   });
 });
