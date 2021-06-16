8dea6b569ffe4cdf45b4f64558fa695460f10956
valeri <valeritsert@gmail.com>
2021-06-16 09:30:21 +0400

    components/OrderList: add padding to items

--- components/OrderList.tsx
+++ components/OrderList.tsx
@@ -39,6 +39,7 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
+    paddingVertical: 4,
   },
   white: {
     color: "#fff",
