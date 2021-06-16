625145c95784d9f614a1b7af27ecdaf6ba1ceadb
valeri <valeritsert@gmail.com>
2021-06-16 11:11:25 +0400

    components/OrderList: set zIndex for ratio bar

--- components/OrderList.tsx
+++ components/OrderList.tsx
@@ -86,6 +86,7 @@ const styles = StyleSheet.create({
     right: 0,
     top: 0,
     height: "100%",
+    zIndex: -1,
   },
 
   askTotal: {
@@ -94,5 +95,6 @@ const styles = StyleSheet.create({
     right: 0,
     top: 0,
     height: "100%",
+    zIndex: -1,
   },
 });
