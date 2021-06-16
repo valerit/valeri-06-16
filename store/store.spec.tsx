ab3fbba8e3eb60235aeb68ff2b06355952ab9da4
valeri <valeritsert@gmail.com>
2021-06-16 22:06:30 +0400

    store/store.spec: test updates

--- store/store.spec.tsx
+++ store/store.spec.tsx
@@ -1,11 +1,18 @@
 import Store from "./index";
 Store.groupOffset = 1;
 
-const msg = `{
+const msg1 = `{
   "feed":"book_ui_1",
   "product_id":"PI_XBTUSD",
   "bids":[[1, 1],[1.5, 1], [3, 2], [3.5, 3]],
   "asks":[[1, 1],[1.5, 1], [4, 3], [4.5, 4]]
+}`;
+
+const msg2 = `{
+  "feed":"book_ui_1",
+  "product_id":"PI_XBTUSD",
+  "bids":[[1, 0],[1.5, 1], [3, 0], [3.5, 7]],
+  "asks":[[1, 0],[1.5, 1], [4, 3], [4.5, 4]]
 }`;
 
 describe("Store", () => {
@@ -16,7 +23,7 @@ describe("Store", () => {
   });
 
   it("should group data correctly", () => {
-    const data = JSON.parse(msg);
+    const data = JSON.parse(msg1);
     Store.onOrderMsg(data);
     Store.refresh();
 
@@ -27,5 +34,19 @@ describe("Store", () => {
     expect(Store.asks.length).toBe(2);
     expect(Store.asks[0].price).toBe(1);
     expect(Store.asks[0].size).toBe(2);
+  });
+
+  it("should calculate updates correctly", () => {
+    const data = JSON.parse(msg2);
+    Store.onOrderMsg(data);
+    Store.refresh();
+
+    expect(Store.bids.length).toBe(2);
+    expect(Store.bids[0].price).toBe(3);
+    expect(Store.bids[0].size).toBe(7);
+
+    expect(Store.asks.length).toBe(2);
+    expect(Store.asks[0].price).toBe(1);
+    expect(Store.asks[0].size).toBe(1);
   });
 });
