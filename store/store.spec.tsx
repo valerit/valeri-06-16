0f435619ff5e5c36952f139f7cb54d7ae0df8e68
valeri <valeritsert@gmail.com>
2021-06-16 22:14:57 +0400

    store/store.spec: test for unexpected messages

--- store/store.spec.tsx
+++ store/store.spec.tsx
@@ -15,6 +15,20 @@ const msg2 = `{
   "asks":[[1, 0],[1.5, 1], [4, 3], [4.5, 4]]
 }`;
 
+const invalidMsg1 = `{
+  "feed":"book_ui_1",
+  "product_id":"PI_XBTUSD",
+  "bids":[[1, 0],[1.5, 1], [3, 0], 3],
+  "asks":[[1, 0],[1.5, 1], [4, 3], "aaaa"]
+}`;
+
+const invalidMsg2 = `{
+  "feed":"book_ui_1",
+  "product_id":"PI_XBTUSD",
+  "bids":5,
+  "asks":[[1, 0],[1.5, 1], [4, 3], "aaaa"]
+}`;
+
 describe("Store", () => {
   it("should get a correct group price", () => {
     const groupPrice = Store.getGroupPrice(1.5);
@@ -48,5 +62,16 @@ describe("Store", () => {
     expect(Store.asks.length).toBe(2);
     expect(Store.asks[0].price).toBe(1);
     expect(Store.asks[0].size).toBe(1);
+  });
+
+  it("should handle unexpected messages", () => {
+    const data1 = JSON.parse(invalidMsg1);
+    const data2 = JSON.parse(invalidMsg2);
+    Store.onOrderMsg(data1);
+    Store.refresh();
+    Store.onOrderMsg(data2);
+    Store.refresh();
+
+    expect(true).toBe(true);
   });
 });
